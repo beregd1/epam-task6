4b3a67cd1629b385bff78c55146ca519d8456c1d
Dimon <beregd2013@gmail.com>
2021-04-21 15:28:43 +0300

    2

--- src/main/java/com/epam/rd/java/basic/practice6/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice6/Demo.java
@@ -1,13 +1,21 @@
 package com.epam.rd.java.basic.practice6;
 
 import com.epam.rd.java.basic.practice6.part1.Part1;
+import com.epam.rd.java.basic.practice6.part2.Part2;
+import com.epam.rd.java.basic.practice6.part3.Part3;
+import com.epam.rd.java.basic.practice6.part4.Part4;
 
 public class Demo {
 
 	public static void main(String[] args) {
 System.out.println("Part1: ");
 		Part1.main(null);
-	    
+		System.out.println("Part2: ");
+		Part2.main(null);
+		System.out.println("Part3: ");
+		Part3.main(null);
+		System.out.println("Part4: ");
+		Part4.main(null);
 	}
 
 }
