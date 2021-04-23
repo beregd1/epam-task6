d03b95f15c613b9c76f05cde51bb050bae293e22
Dimon <beregd2013@gmail.com>
2021-04-23 11:46:48 +0300

    3

--- src/main/java/com/epam/rd/java/basic/practice6/part5/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part5/Part5.java
@@ -1,9 +1,27 @@
 package com.epam.rd.java.basic.practice6.part5;
 
 public class Part5 {
-    
+
     public static void main(String[] args) {
-        
+
+        Tree<Integer> tree = new Tree<>();
+
+        System.out.println(tree.add(3));
+        System.out.println(tree.add(7));
+        System.out.println(tree.add(7));
+        System.out.println(tree.add(8));
+        System.out.println(tree.add(6));
+        System.out.println(tree.add(2));
+        System.out.println(tree.add(5));
+
+        tree.print();
+
+        System.out.println("----------------------------");
+
+        System.out.println(tree.remove(7));
+
+        tree.print();
+
     }
-    
+
 }
